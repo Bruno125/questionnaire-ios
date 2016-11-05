9ac8ce82cf88f67c68b8842cd464ad01f5c73ffa
Bruno Aybar <miguel_bruno@hotmail.com>
2016-11-05 16:10:24 +0000

    Add parsing func to question model

--- Questionnaire/Models/Question.swift
+++ Questionnaire/Models/Question.swift
@@ -7,16 +7,42 @@
 //
 
 import UIKit
+import SwiftyJSON
 
 class Choice{
-    let label : Int = 0
+    let label : String
     var value : Int = 0
+    
+    init(label: String, value :Int) {
+        self.label = label
+        self.value = value
+    }
 }
 
 class Question: NSObject {
-    let name: String = ""
-    let type: String = ""
-    let title: String = ""
-    let question: String = ""
+    let name: String
+    let type: String
+    let title: String
+    
+    let choices: [Choice]
+    
+    init(name: String, type: String, title :String, choices :[Choice]) {
+        self.name = name
+        self.type = type
+        self.title = title
+        self.choices = choices
+    }
+    
+    static func parse(json : JSON) -> Question{
+        let name = json["name"].stringValue
+        let type = json["type"].stringValue
+        let title = json["title"].stringValue
+        
+        let choices = json["choices"].arrayValue.map({
+            Choice(label: $0["label"].stringValue, value: $0["label"].intValue)
+        })
+        
+        return Question(name: name, type: type, title: title, choices: choices)
+    }
     
 }
